7dd67aba2379147a690d1d7c7fa60435f2c0a363
zdzulak <43354865+zdzulak@users.noreply.github.com>
2019-04-17 10:20:53 -0400

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 
 // global database connection
-$conn = new PDO('mysql:host=localhost;dbname=cooldb', 'root', '');
+$conn = new PDO('mysql:host=127.0.0.1:54980;dbname=cooldb', 'root', '');
 
 ?>
